4b74dc05ec1183030aab4e3ed54fb86b95af1fd3
Jan Hancic <jan.hancic@gmail.com>
2014-08-16 16:06:26 +0100

    add error check for num of childeren

--- src/react-tooltip.js
+++ src/react-tooltip.js
@@ -7,6 +7,16 @@ var React = require('react');
 var ToolTip = React.createClass({
 	displayName: 'ToolTip',
 
+	componentWillMount: function() {
+		var numOfChildren = React.Children.count(this.props.children);
+
+		if (numOfChildren === 0) {
+			throw new Error('ToolTip must contain a child element/component.');
+		} else if (numOfChildren > 1) {
+			throw new Error('ToolTip can contain only one child element/component.');
+		}
+	},
+
 	render: function() {
 		return (
 			<div>Hello ToolTip</div>

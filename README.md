401536fca492bb8fcd53b626f023e1fcb7bb28a0
Jan Hancic <jan.hancic@gmail.com>
2014-08-16 15:48:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Tooltip Component
+
+An experiment to try to figure out how best to display tooltips with React.
+
+## License
+See `LICENSE` file.
---
+++ src/react-tooltip.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var ToolTip = React.createClass({
+	displayName: 'ToolTip',
+
+	render: function() {
+		return (
+			<div>Hello ToolTip</div>
+		);
+	}
+});
+
+module.exports = ToolTip;
